3a51a364de2dd33157abbad95e88fa0124917e0d
John Afolabi <johnafolabi.da@gmail.com>
2019-11-12 12:39:51 +0100

    Update project dependencies (#780)
    * Update dependencies
    
    * Generate new lockfile
    
    * Patch version bump
    
    * Yarn update to actually generate new lockfile and update dependencies
    
    * React Router is now on major version 5

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import { fromByteArray } from 'ipaddr.js';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +14,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList}/>
+      <Route exact path="/movies/:id" component={Movie}/>
     </div>
   );
 };
